cf38fb1d66146b81a1264c9f9df46157618f595e
appsynccreator <appsync.creator@gmail.com>
2026-05-11 12:50:26 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+// ============================================================
+// LOKARA - FILE KONFIGURASI
+// Edit file ini untuk mengubah URL Google Apps Script
+// File ini TIDAK perlu diubah saat update index.html
+// ============================================================
+
+var LOKARA_CONFIG = {
+
+  // Ganti dengan URL deploy Google Apps Script kamu
+  // Contoh: 'https://script.google.com/macros/s/AKfycb.../exec'
+  GAS_URL: 'GANTI_DENGAN_URL_GAS_KAMU'
+
+};
